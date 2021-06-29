e87f2abd77c1e740844121dce4205a48b47e5ccd
lburdock <lauren.burdock@gmail.com>
2021-06-28 21:11:57 -0500

    Deploy website - based on b9934d4db46adf85427b0c8240494f49123a8964

--- src/components/external-link/link.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function ExternalLink({ children, ...props }) {
-  return (
-    <a rel="noopener noreferrer" target="_blank" {...props}>
-      {children}
-    </a>
-  );
-}
